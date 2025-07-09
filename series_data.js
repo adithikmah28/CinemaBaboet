950f063296aa002e5ee66b40b534585d2d1e2391
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 22:49:06 +0700

    Create series_data.js

---
+++ series_data.js
@@ -0,0 +1,25 @@
+const seriesData = [
+    {
+        id: 108545,
+        title: 'Gadis Kretek',
+        year: '2023',
+        category: 'series',
+        quality: 'HD',
+        rating: 8.5,
+        genre: ['Drama', 'Sejarah', 'Romantis'],
+        poster: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
+        poster_lg: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
+        synopsis: 'Kisah Dasiyah, seorang wanita visioner yang gemar meracik saus rokok kretek, berlatar belakang industri tembakau di Indonesia.',
+        cast: ['Dian Sastrowardoyo', 'Ario Bayu', 'Putri Marino'],
+        director: 'Kamila Andini, Ifa Isfansyah',
+        seasons: [
+            {
+                season_number: 1,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/dQw4w9WgXcQ' },
+                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/uYPbbksJxIg' }
+                ]
+            }
+        ]
+    }
+];
