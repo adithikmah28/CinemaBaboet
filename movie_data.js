e6d63c11845b0b7dabb64c6238ab09283bbf07f3
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 22:48:49 +0700

    Create movie_data.js

---
+++ movie_data.js
@@ -0,0 +1,18 @@
+const movieData = [
+    {
+        id: 872585,
+        title: 'Oppenheimer',
+        year: '2023',
+        category: 'movie',
+        quality: 'HD',
+        rating: 8.6,
+        genre: ['Biografi', 'Drama', 'Sejarah'],
+        poster: 'https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/fm6KqXpk3M2HVveHwCrBSSBaO0V.jpg',
+        synopsis: 'Kisah fisikawan teoretis J. Robert Oppenheimer, yang memimpin Proyek Manhattan untuk mengembangkan bom atom pertama bagi Amerika Serikat selama Perang Dunia II.',
+        cast: ['Cillian Murphy', 'Emily Blunt', 'Matt Damon', 'Robert Downey Jr.'],
+        director: 'Christopher Nolan',
+        subtitle: 'English, Indonesia',
+        iframeSrc: 'https://www.youtube.com/embed/uYPbbksJxIg'
+    }
+];
