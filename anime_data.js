9677eccdce731282385d254dda30dab1a2087531
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 22:49:48 +0700

    Create anime_data.js

---
+++ anime_data.js
@@ -0,0 +1,32 @@
+const animeData = [
+    {
+        id: 1429,
+        title: 'Attack on Titan',
+        year: '2013',
+        category: 'anime',
+        quality: 'HD',
+        rating: 9.1,
+        genre: ['Aksi', 'Petualangan', 'Animasi', 'Fantasi'],
+        poster: 'https://image.tmdb.org/t/p/w500/hTP1DtLGFamjfu8WqjnuQ5116fX.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/r1b0n53IoA22i0c3n2l1W093I6E.jpg',
+        synopsis: 'Setelah kampung halamannya hancur, Eren Yeager muda bersumpah untuk membersihkan bumi dari para Titan humanoid raksasa yang telah membawa umat manusia ke ambang kepunahan.',
+        cast: ['Yuki Kaji', 'Yui Ishikawa', 'Marina Inoue'],
+        director: 'Tetsurō Araki',
+        seasons: [
+            {
+                season_number: 1,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/MGRm4IzK1SQ' },
+                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/MGRm4IzK1SQ' }
+                ]
+            },
+            {
+                season_number: 2,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/MGRm4IzK1SQ' },
+                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/MGRm4IzK1SQ' }
+                ]
+            }
+        ]
+    }
+];
