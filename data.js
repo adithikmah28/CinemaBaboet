4eb26c79ef317d2d772d192ec6bcb9a1488aaae8
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 16:48:23 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+// DATABASE ULTIMATE - Dengan data yang 100% LENGKAP untuk setiap entri
 const allContent = [
     {
         id: 1,
@@ -8,9 +9,18 @@ const allContent = [
         rating: 8.5,
         poster: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
         poster_lg: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
-        synopsis: 'Kisah Dasiyah, seorang wanita visioner yang gemar meracik saus rokok kretek, berlatar belakang industri tembakau di Indonesia.',
+        synopsis: 'Kisah Dasiyah, seorang wanita visioner yang gemar meracik saus rokok kretek, berlatar belakang industri tembakau di Indonesia pada tahun 1960-an.',
+        cast: ['Dian Sastrowardoyo', 'Ario Bayu', 'Putri Marino'],
+        director: 'Kamila Andini, Ifa Isfansyah',
         seasons: [
-            { season_number: 1, episodes: [ { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/dQw4w9WgXcQ' }, { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/uYPbbksJxIg' } ] }
+            {
+                season_number: 1,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/dQw4w9WgXcQ' },
+                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/uYPbbksJxIg' },
+                    { episode_number: 3, iframeSrc: 'https://www.youtube.com/embed/9B69pS05S8w' }
+                ]
+            }
         ]
     },
     {
@@ -23,6 +33,9 @@ const allContent = [
         poster: 'https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_FMjpg_UX1000_.jpg',
         poster_lg: 'https://images.thedirect.com/media/photos/oppenheimer-poster.jpg',
         synopsis: 'Kisah fisikawan teoretis Amerika J. Robert Oppenheimer, dan kontribusinya dalam penciptaan bom atom.',
+        cast: ['Cillian Murphy', 'Emily Blunt', 'Matt Damon', 'Robert Downey Jr.'],
+        director: 'Christopher Nolan',
+        subtitle: 'English, Indonesia',
         iframeSrc: 'https://www.youtube.com/embed/uYPbbksJxIg'
     },
     {
@@ -35,6 +48,9 @@ const allContent = [
         poster: 'https://m.media-amazon.com/images/M/MV5BODkxNWM1MjYtZWZlZC00M2I0LWIzYWMtOTk3YTRkZDYxZDMxXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg',
         poster_lg: 'https://m.media-amazon.com/images/M/MV5BODkxNWM1MjYtZWZlZC00M2I0LWIzYWMtOTk3YTRkZDYxZDMxXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg',
         synopsis: 'Empat sekawan pengelola rumah hantu harus menghadapi konsekuensi saat salah satu pengunjung meninggal karena kaget.',
+        cast: ['Bene Dion', 'Oki Rengga', 'Indra Jegel', 'Boris Bokir'],
+        director: 'Muhadkly Acho',
+        subtitle: 'Indonesia',
         iframeSrc: 'https://www.youtube.com/embed/9B69pS05S8w'
     }
 ];
