7fc1875a1bc8d58afc1e3913f71816328438f55a
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 22:49:28 +0700

    Create indonesia_data.js

---
+++ indonesia_data.js
@@ -0,0 +1,18 @@
+const indonesiaData = [
+    {
+        id: 1133223,
+        title: 'Agak Laen',
+        year: '2024',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 8.9,
+        genre: ['Komedi', 'Horor'],
+        poster: 'https://m.media-amazon.com/images/M/MV5BODkxNWM1MjYtZWZlZC00M2I0LWIzYWMtOTk3YTRkZDYxZDMxXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg',
+        poster_lg: 'https://m.media-amazon.com/images/M/MV5BODkxNWM1MjYtZWZlZC00M2I0LWIzYWMtOTk3YTRkZDYxZDMxXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg',
+        synopsis: 'Empat sekawan pengelola rumah hantu harus menghadapi konsekuensi saat salah satu pengunjung meninggal karena kaget.',
+        cast: ['Bene Dion', 'Oki Rengga', 'Indra Jegel', 'Boris Bokir'],
+        director: 'Muhadkly Acho',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://www.youtube.com/embed/9B69pS05S8w'
+    }
+];
