5bd17cfe1ff94572adedc30298d6b50310d68749
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 22:52:25 +0700

    Update series_data.js

--- series_data.js
+++ series_data.js
@@ -1,23 +1,53 @@
 const seriesData = [
     {
-        id: 108545,
-        title: 'Gadis Kretek',
-        year: '2023',
+        id: 93405,
+        title: 'Squid Game',
+        year: '2021',
         category: 'series',
         quality: 'HD',
-        rating: 8.5,
-        genre: ['Drama', 'Sejarah', 'Romantis'],
-        poster: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
-        poster_lg: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
-        synopsis: 'Kisah Dasiyah, seorang wanita visioner yang gemar meracik saus rokok kretek, berlatar belakang industri tembakau di Indonesia.',
-        cast: ['Dian Sastrowardoyo', 'Ario Bayu', 'Putri Marino'],
-        director: 'Kamila Andini, Ifa Isfansyah',
+        rating: 7.9,
+        genre: ['Action', 'Mystery', 'Drama'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/original/1QdXdRYfktUSONkl1oD5gc6Be0s.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/1QdXdRYfktUSONkl1oD5gc6Be0s.jpg',
+        synopsis: 'Ratusan pemain yang butuh uang menerima undangan aneh untuk bertanding dalam permainan anak-anak. Di permainan ini, hadiah menggiurkan menanti—dengan taruhan mematikan.',
+        cast: ['Lee Jung-jae', 'Wi Ha-jun', 'Jeon Young-soo'],
+        director: 'Hwang Dong-hyuk',
         seasons: [
             {
                 season_number: 1,
                 episodes: [
-                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/dQw4w9WgXcQ' },
-                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/uYPbbksJxIg' }
+                    { episode_number: 1, iframeSrc: 'https://vidlink.pro/tv/93405/1/1' },
+                    { episode_number: 2, iframeSrc: 'https://vidlink.pro/tv/93405/1/2' },
+                    { episode_number: 3, iframeSrc: 'https://vidlink.pro/tv/93405/1/3' },
+                    { episode_number: 4, iframeSrc: 'https://vidlink.pro/tv/93405/1/4' },
+                    { episode_number: 5, iframeSrc: 'https://vidlink.pro/tv/93405/1/5' },
+                    { episode_number: 6, iframeSrc: 'https://vidlink.pro/tv/93405/1/6' },
+                    { episode_number: 7, iframeSrc: 'https://vidlink.pro/tv/93405/1/7' },
+                    { episode_number: 8, iframeSrc: 'https://vidlink.pro/tv/93405/1/8' },
+                    { episode_number: 9, iframeSrc: 'https://vidlink.pro/tv/93405/1/9' },
+                    ]
+            },
+            {
+                season_number: 2,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://vidlink.pro/tv/93405/2/1' },
+                    { episode_number: 2, iframeSrc: 'https://vidlink.pro/tv/93405/2/2' },
+                    { episode_number: 3, iframeSrc: 'https://vidlink.pro/tv/93405/2/3' },
+                    { episode_number: 4, iframeSrc: 'https://vidlink.pro/tv/93405/2/4' },
+                    { episode_number: 5, iframeSrc: 'https://vidlink.pro/tv/93405/2/5' },
+                    { episode_number: 6, iframeSrc: 'https://vidlink.pro/tv/93405/2/6' },
+                    { episode_number: 7, iframeSrc: 'https://vidlink.pro/tv/93405/2/7' },
+                    ]
+            },
+            {
+                season_number: 3,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://vidlink.pro/tv/93405/3/1' },
+                    { episode_number: 2, iframeSrc: 'https://vidlink.pro/tv/93405/3/2' },
+                    { episode_number: 3, iframeSrc: 'https://vidlink.pro/tv/93405/3/3' },
+                    { episode_number: 4, iframeSrc: 'https://vidlink.pro/tv/93405/3/4' },
+                    { episode_number: 5, iframeSrc: 'https://vidlink.pro/tv/93405/3/5' },
+                    { episode_number: 6, iframeSrc: 'https://vidlink.pro/tv/93405/3/6' }
                 ]
             }
         ]
