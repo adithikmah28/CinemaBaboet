3187ec6ed6487933a824f140997326051a70bf44
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 21:46:55 +0700

    Update data.js

--- data.js
+++ data.js
@@ -100,5 +100,53 @@ const allContent = [
         director: 'Joko Anwar',
         subtitle: 'Indonesia',
         iframeSrc: 'https://vidlink.pro/movie/572468'
+    },
+    {
+        id: 5,
+        title: 'Pengabdi Setan',
+        year: '2017',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 6.7,
+        genre: ['Drama', 'Horror', 'Mystery'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/original/xTWgncjZOrMmSjaEwA3DLOxaqf0.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/xTWgncjZOrMmSjaEwA3DLOxaqf0.jpg',
+        synopsis: 'Setelah Rini ditinggal mati ibunya, ada sesuatu peristiwa yang terus menganggu keluarga mereka.',
+        cast: ['Tara Basro', 'Bront Palarae', 'Endy Arfian'],
+        director: 'Joko Anwar',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidlink.pro/movie/467012'
+    },
+    {
+        id: 6,
+        title: 'pengabdi setan 2: communion',
+        year: '2022',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 6.9,
+        genre: ['Drama', 'Horror', 'Mystery'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/original/zzpxuEXS1azwhqfvtacLtScixbS.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/zzpxuEXS1azwhqfvtacLtScixbS.jpg',
+        synopsis: 'Beberapa tahun setelah berhasil menyelamatkan diri dari kejadian mengerikan yang membuat mereka kehilangan ibu dan si bungsu Ian, Rini dan adik-adiknya, Toni dan Bondi, serta Bapak tinggal di rumah susun karena percaya tinggal di rumah susun aman jika terjadi sesuatu karena ada banyak orang. Namun, mereka segera menyadari bahwa tinggal bersama banyak orang mungkin juga sangat berbahaya, jika mereka tidak sangat mengenali siapa saja yang menjadi tetangga mereka. Pada sebuah malam penuh teror, Rini dan Keluarganya harus kembali menyelamatkan diri. Tapi kali ini, mungkin sudah terlambat untuk lari.',
+        cast: ['Tara Basro', 'Bront Palarae', 'Endy Arfian'],
+        director: 'Joko Anwar',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidlink.pro/movie/925786'
+    },
+    {
+        id: 7,
+        title: 'Mencuri Raden Saleh',
+        year: '2022',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 7.5,
+        genre: ['Drama', 'Horror', 'Mystery'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/1fdqRcgEYNkXCnnKAzz2JtFnUv7.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/1fdqRcgEYNkXCnnKAzz2JtFnUv7.jpg',
+        synopsis: 'Mengisahkan tentang pencurian lukisan karya Raden Saleh.',
+        cast: ['Iqbaal Ramadhan', 'Angga Yunanda', 'Aghniny Haque'],
+        director: 'Angga Dwimas Sasongko',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidsrc.icu/embed/movie/832544'
     }
 ];
