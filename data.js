773cac98716747fa500d35319cb08b5aee994a91
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 19:36:39 +0700

    Update data.js

--- data.js
+++ data.js
@@ -25,7 +25,30 @@ const allContent = [
                     { episode_number: 6, iframeSrc: 'https://vidlink.pro/tv/93405/1/6' },
                     { episode_number: 7, iframeSrc: 'https://vidlink.pro/tv/93405/1/7' },
                     { episode_number: 8, iframeSrc: 'https://vidlink.pro/tv/93405/1/8' },
-                    { episode_number: 9, iframeSrc: 'https://vidlink.pro/tv/93405/1/9' }
+                    { episode_number: 9, iframeSrc: 'https://vidlink.pro/tv/93405/1/9' },
+                    ]
+            },
+            {
+                season_number: 2,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://vidlink.pro/tv/93405/2/1' },
+                    { episode_number: 2, iframeSrc: 'https://vidlink.pro/tv/93405/2/2' },
+                    { episode_number: 3, iframeSrc: 'https://vidlink.pro/tv/93405/2/3' },
+                    { episode_number: 4, iframeSrc: 'https://vidlink.pro/tv/93405/2/4' },
+                    { episode_number: 5, iframeSrc: 'https://vidlink.pro/tv/93405/2/5' },
+                    { episode_number: 6, iframeSrc: 'https://vidlink.pro/tv/93405/2/6' },
+                    { episode_number: 7, iframeSrc: 'https://vidlink.pro/tv/93405/2/7' },
+                    ]
+            },
+            {
+                season_number: 3,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://vidlink.pro/tv/93405/3/1' },
+                    { episode_number: 2, iframeSrc: 'https://vidlink.pro/tv/93405/3/2' },
+                    { episode_number: 3, iframeSrc: 'https://vidlink.pro/tv/93405/3/3' },
+                    { episode_number: 4, iframeSrc: 'https://vidlink.pro/tv/93405/3/4' },
+                    { episode_number: 5, iframeSrc: 'https://vidlink.pro/tv/93405/3/5' },
+                    { episode_number: 6, iframeSrc: 'https://vidlink.pro/tv/93405/3/6' }
                 ]
             }
         ]
