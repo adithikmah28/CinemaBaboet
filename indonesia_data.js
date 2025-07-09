8e009935dbbe5938533f52ee621cee89ce918fa4
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 22:55:09 +0700

    Update indonesia_data.js

--- indonesia_data.js
+++ indonesia_data.js
@@ -14,5 +14,85 @@ const indonesiaData = [
         director: 'Hadrah Daeng Ratu',
         subtitle: 'Indonesia',
         iframeSrc: 'https://vidlink.pro/movie/1223799'
+    },
+    {
+        id: 572468,
+        title: 'Perempuan Tanah Jahanam',
+        year: '2019',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 6.7,
+        genre: ['Horror', 'Mystery', 'Thriller'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/original/tAYHRuKxNa4arD32YTmDT1j46Mi.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/tAYHRuKxNa4arD32YTmDT1j46Mi.jpg',
+        synopsis: 'Seorang operator pintu tol pergi ke kampung leluhurnya dengan harapan mendapatkan harta warisan, hingga akhirnya ia menemukan fakta yang meresahkan tentang masa lalunya.',
+        cast: ['Tara Basro', 'Marissa Anita', 'Asmara Abigail'],
+        director: 'Joko Anwar',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidlink.pro/movie/572468'
+    },
+    {
+        id: 467012,
+        title: 'Pengabdi Setan',
+        year: '2017',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 6.7,
+        genre: ['Drama', 'Horror', 'Mystery'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/original/xTWgncjZOrMmSjaEwA3DLOxaqf0.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/xTWgncjZOrMmSjaEwA3DLOxaqf0.jpg',
+        synopsis: 'Setelah Rini ditinggal mati ibunya, ada sesuatu peristiwa yang terus menganggu keluarga mereka.',
+        cast: ['Tara Basro', 'Bront Palarae', 'Endy Arfian'],
+        director: 'Joko Anwar',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidlink.pro/movie/467012'
+    },
+    {
+        id: 925786,
+        title: 'pengabdi setan 2: communion',
+        year: '2022',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 6.9,
+        genre: ['Drama', 'Horror', 'Mystery'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/original/zzpxuEXS1azwhqfvtacLtScixbS.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/original/zzpxuEXS1azwhqfvtacLtScixbS.jpg',
+        synopsis: 'Beberapa tahun setelah berhasil menyelamatkan diri dari kejadian mengerikan yang membuat mereka kehilangan ibu dan si bungsu Ian, Rini dan adik-adiknya, Toni dan Bondi, serta Bapak tinggal di rumah susun karena percaya tinggal di rumah susun aman jika terjadi sesuatu karena ada banyak orang. Namun, mereka segera menyadari bahwa tinggal bersama banyak orang mungkin juga sangat berbahaya, jika mereka tidak sangat mengenali siapa saja yang menjadi tetangga mereka. Pada sebuah malam penuh teror, Rini dan Keluarganya harus kembali menyelamatkan diri. Tapi kali ini, mungkin sudah terlambat untuk lari.',
+        cast: ['Tara Basro', 'Bront Palarae', 'Endy Arfian'],
+        director: 'Joko Anwar',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidlink.pro/movie/925786'
+    },
+    {
+        id: 832544,
+        title: 'Mencuri Raden Saleh',
+        year: '2022',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 7.5,
+        genre: ['Drama', 'Horror', 'Mystery'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/1fdqRcgEYNkXCnnKAzz2JtFnUv7.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/1fdqRcgEYNkXCnnKAzz2JtFnUv7.jpg',
+        synopsis: 'Mengisahkan tentang pencurian lukisan karya Raden Saleh.',
+        cast: ['Iqbaal Ramadhan', 'Angga Yunanda', 'Aghniny Haque'],
+        director: 'Angga Dwimas Sasongko',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidsrc.pm/embed/movie?tmdb=832544'
+    },
+    {
+        id: 480007,
+        title: 'mata batin',
+        year: '2018',
+        category: 'indonesia',
+        quality: 'HD',
+        rating: 7.5,
+        genre: ['Drama', 'Horror', 'Mystery'], // <-- GENRE DITAMBAHKAN
+        poster: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9vDSQoEVFCwt9O21E0O0BEqu57k.jpg',
+        poster_lg: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9vDSQoEVFCwt9O21E0O0BEqu57k.jpg',
+        synopsis: 'Seorang wanita tersiksa dengan kemampuan mata ketiga.',
+        cast: ['Jessica Mila', 'Bianca Hello', 'Denny Sumargo'],
+        director: 'Rocky Soraya',
+        subtitle: 'Indonesia',
+        iframeSrc: 'https://vidsrc.pm/embed/movie?tmdb=480007'
     }
 ];
