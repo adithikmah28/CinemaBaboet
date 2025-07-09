bee8d2f874d64a3c3b4cc0e94fd0e492ed29774f
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-09 16:19:44 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,70 @@
+// DATABASE ULTIMATE - Dengan struktur Season & Episode untuk Series
 const allContent = [
+    {
+        id: 1,
+        title: 'Gadis Kretek',
+        year: '2023',
+        category: 'series', // Kategori Series
+        quality: 'HD',
+        rating: 8.5,
+        poster: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
+        poster_lg: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
+        synopsis: 'Kisah Dasiyah, seorang wanita visioner yang gemar meracik saus rokok kretek, berlatar belakang industri tembakau di Indonesia.',
+        cast: ['Dian Sastrowardoyo', 'Ario Bayu', 'Putri Marino'],
+        director: 'Kamila Andini',
+        // STRUKTUR BARU UNTUK SERIES
+        seasons: [
+            {
+                season_number: 1,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/dQw4w9WgXcQ?si=example1' },
+                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/uYPbbksJxIg?si=example2' },
+                    { episode_number: 3, iframeSrc: 'https://www.youtube.com/embed/9B69pS05S8w?si=example3' },
+                    { episode_number: 4, iframeSrc: 'https://www.youtube.com/embed/Y274jZs5s7s?si=example4' },
+                    { episode_number: 5, iframeSrc: 'https://www.youtube.com/embed/b9EkMc79ZSU?si=example5' }
+                ]
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'Stranger Things',
+        year: '2016',
+        category: 'series',
+        quality: 'HD',
+        rating: 8.7,
+        poster: 'https://m.media-amazon.com/images/M/MV5BNDU4ODQ0OTk3OF5BMl5BanBnXkFtZTgwOTA4MDY2NTE@._V1_FMjpg_UX1000_.jpg',
+        poster_lg: 'https://www.xtrafondos.com/wallpapers/resized/elenco-de-stranger-things-4-10617.jpg?s=large',
+        synopsis: 'Ketika seorang anak laki-laki menghilang, sebuah kota kecil mengungkap misteri yang melibatkan eksperimen rahasia dan kekuatan supernatural.',
+        cast: ['Millie Bobby Brown', 'Finn Wolfhard', 'Winona Ryder'],
+        director: 'The Duffer Brothers',
+        seasons: [
+            {
+                season_number: 1,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/b9EkMc79ZSU?si=st1ep1' },
+                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/b9EkMc79ZSU?si=st1ep2' },
+                    { episode_number: 3, iframeSrc: 'https://www.youtube.com/embed/b9EkMc79ZSU?si=st1ep3' },
+                    { episode_number: 4, iframeSrc: 'https://www.youtube.com/embed/b9EkMc79ZSU?si=st1ep4' }
+                ]
+            },
+            {
+                season_number: 2,
+                episodes: [
+                    { episode_number: 1, iframeSrc: 'https://www.youtube.com/embed/R1g07RpTPFE?si=st2ep1' },
+                    { episode_number: 2, iframeSrc: 'https://www.youtube.com/embed/R1g07RpTPFE?si=st2ep2' },
+                    { episode_number: 3, iframeSrc: 'https://www.youtube.com/embed/R1g07RpTPFE?si=st2ep3' }
+                ]
+            }
+        ]
+    },
     {
         id: 9,
         title: 'Oppenheimer',
         year: '2023',
-        category: 'movie',
-        isOriginal: true,
-        quality: 'HD', // 'HD', 'SD', 'CAM'
-        rating: 8.6,   // Angka rating
+        category: 'movie', // Kategori Movie
+        quality: 'HD',
+        rating: 8.6,
         poster: 'https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_FMjpg_UX1000_.jpg',
         poster_lg: 'https://images.thedirect.com/media/photos/oppenheimer-poster.jpg',
         synopsis: 'Kisah fisikawan teoretis Amerika J. Robert Oppenheimer, dan kontribusinya dalam penciptaan bom atom.',
@@ -19,8 +77,7 @@ const allContent = [
         id: 3,
         title: 'Agak Laen',
         year: '2024',
-        category: 'indonesia',
-        isOriginal: false,
+        category: 'indonesia', // Kategori Indonesia
         quality: 'HD',
         rating: 8.9,
         poster: 'https://m.media-amazon.com/images/M/MV5BODkxNWM1MjYtZWZlZC00M2I0LWIzYWMtOTk3YTRkZDYxZDMxXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg',
@@ -30,53 +87,5 @@ const allContent = [
         director: 'Muhadkly Acho',
         subtitle: 'Indonesia',
         iframeSrc: 'https://www.youtube.com/embed/9B69pS05S8w?si=t5y-MhT2L3SjB_5r'
-    },
-    {
-        id: 1,
-        title: 'Gadis Kretek',
-        year: '2023',
-        category: 'series',
-        isOriginal: true,
-        quality: 'HD',
-        rating: 8.5,
-        poster: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
-        poster_lg: 'https://m.media-amazon.com/images/M/MV5BN2RjZGVkNDItN2I3NC00YjVjLWIyYTUtZDE4NTA2MGMzODc4XkEyXkFqcGdeQXVyMTEzNDY2ODE5._V1_FMjpg_UX1000_.jpg',
-        synopsis: 'Kisah Dasiyah, seorang wanita visioner yang gemar meracik saus rokok kretek, berlatar belakang industri tembakau di Indonesia.',
-        cast: ['Dian Sastrowardoyo', 'Ario Bayu', 'Putri Marino'],
-        director: 'Kamila Andini',
-        subtitle: 'Indonesia, English',
-        iframeSrc: 'https://www.youtube.com/embed/videoseries?si=c-tXm2LzL7U5q3iM&list=PLgY_4k3g914GdiWa3Wk_Ld3u4nmx3ZJQY'
-    },
-    {
-        id: 4,
-        title: 'Stranger Things',
-        year: '2016',
-        category: 'series',
-        isOriginal: false,
-        quality: 'SD', // Contoh Kualitas SD
-        rating: 7.9,
-        poster: 'https://m.media-amazon.com/images/M/MV5BNDU4ODQ0OTk3OF5BMl5BanBnXkFtZTgwOTA4MDY2NTE@._V1_FMjpg_UX1000_.jpg',
-        poster_lg: 'https://www.xtrafondos.com/wallpapers/resized/elenco-de-stranger-things-4-10617.jpg?s=large',
-        synopsis: 'Ketika seorang anak laki-laki menghilang, sebuah kota kecil mengungkap misteri yang melibatkan eksperimen rahasia dan kekuatan supernatural.',
-        cast: ['Millie Bobby Brown', 'Finn Wolfhard', 'Winona Ryder'],
-        director: 'The Duffer Brothers',
-        subtitle: 'English, Indonesia',
-        iframeSrc: 'https://www.youtube.com/embed/b9EkMc79ZSU?si=4e4Q7E1fJ2I7z7vJ'
-    },
-    {
-        id: 13,
-        title: 'Movie CAM Example',
-        year: '2024',
-        category: 'movie',
-        isOriginal: false,
-        quality: 'CAM', // Contoh Kualitas CAM
-        rating: 4.2,
-        poster: 'https://picsum.photos/id/111/400/600',
-        poster_lg: 'https://picsum.photos/id/111/1920/1080',
-        synopsis: 'Ini adalah contoh film dengan kualitas CAM yang akan menampilkan tag merah.',
-        cast: ['Actor A', 'Actor B'],
-        director: 'Director C',
-        subtitle: 'Indonesia',
-        iframeSrc: 'https://www.youtube.com/embed/dQw4w9WgXcQ'
     }
 ];
